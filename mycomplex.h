268673fd66d8332871814555505899b8ba2b310b
Milana-h751 <82494415+Milana-h751@users.noreply.github.com>
2021-04-14 15:46:51 +0500

    Add files via upload

--- mycomplex.h
+++ mycomplex.h
@@ -1,21 +1,21 @@
 #ifndef _MY_COMPLEX_H_
-#define _MY_COMPLEX_H_
+#define _MY_COMPLEX_H_///<Library for working with complex numbers
 #include <iostream>
 
 using namespace std;
 
-class Complex
+class Complex//class creation Complex
 
 {
       double Re;
 	  double Im;
 public:
 
-      Complex (double aRe = 0, double aIm = 0);
+      Complex (double aRe = 0, double aIm = 0);///Create complex number
       Complex (const Complex & );
      ~Complex();
 
-void Set( double aRe, double aIm = 0);
+void Set( double aRe, double aIm = 0);/// Assigns the complex number a new value.
       operator double();
       double abs();
 
@@ -34,7 +34,7 @@ void Set( double aRe, double aIm = 0);
       friend Complex operator * (const double &, const Complex &);
       Complex operator / (const double &);
 
-      Complex & operator += (const Complex &);
+      Complex & operator += (const Complex &);/// Adds a number to the target complex number, where the number to add is complex.
       Complex & operator -= (const Complex &);
       Complex & operator *= (const Complex &);
       Complex & operator += (const double &);
